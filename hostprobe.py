a3055d2646f324fdcd884762d10a1a88eeb1f95e
Derrick Tilsner <dtt13@case.edu>
2013-11-22 01:56:42 +0000

    added function to extract data from icmp response message

--- hostprobe.py
+++ hostprobe.py
@@ -1,8 +1,22 @@
-
+import random
+import string
 import socket
 
 def getNextTTL():
-	return 3
+	return 5
+
+def getRandomId():
+	return ''.join(random.choice(string.lowercase) for i in range(32))
+
+def extractICMP(icmp_rsp):
+	icmp_type = ord(icmp_rsp[20])
+	icmp_code = ord(icmp_rsp[21])
+	icmp_src = "%d.%d.%d.%d" % (ord(icmp_rsp[12]), ord(icmp_rsp[13]),
+		ord(icmp_rsp[14]), ord(icmp_rsp[15]))
+	icmp_data = icmp_rsp[56:]
+	print "%d, %d src: %s" % (icmp_type, icmp_code, icmp_src)
+	print icmp_data
+	return (icmp_type, icmp_code, icmp_src, icmp_data)
 
 def main(dest_host):
 	dest_addr = socket.gethostbyname(dest_host)
@@ -16,10 +30,12 @@ def main(dest_host):
 		sendSock.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
 
 		# send a short message to the destination
-		sendSock.sendto("hello world", (dest_addr, port))
+		identifier = getRandomId()
+		sendSock.sendto(identifier, (dest_addr, port))
 		
 		icmp_data = icmpSock.recv(512)
-		print icmp_data
+		extractICMP(icmp_data)
+		# print icmp_data
 
 		sendSock.close()
 		icmpSock.close()
