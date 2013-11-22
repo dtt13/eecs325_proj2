6dd81b45d7b704d0ff974140834056686dedcf1a
Derrick Tilsner <dtt13@case.edu>
2013-11-22 00:46:36 +0000

    minor fixes; gets icmp message

--- hostprobe.py
+++ hostprobe.py
@@ -5,18 +5,21 @@ def getNextTTL():
 	return 3
 
 def main(dest_host):
-	dest_addr = gethostbyname(dest_host)
+	dest_addr = socket.gethostbyname(dest_host)
 	port = 50254
 	while True:
 		# set up sockets
-		sendSock = socket.socket(socket.AF_INET, socket.DGRAM)
-		icmpSock = socket.socket(socket.AF_INET, socket.RAW,
+		sendSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		icmpSock = socket.socket(socket.AF_INET, socket.SOCK_RAW,
 			socket.IPPROTO_ICMP)
 		ttl = getNextTTL()
 		sendSock.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
 
 		# send a short message to the destination
 		sendSock.sendto("hello world", (dest_addr, port))
+		
+		icmp_data = icmpSock.recv(512)
+		print icmp_data
 
 		sendSock.close()
 		icmpSock.close()
