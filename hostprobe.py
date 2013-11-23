7b1a4f6ad1a264d81ac4eb2b85bc9e53319f3ae8
Derrick Tilsner <dtt13@case.edu>
2013-11-23 21:07:07 +0000

    fixed getNextTTL() function; still errors with some icmp responses

--- hostprobe.py
+++ hostprobe.py
@@ -2,8 +2,16 @@ import random
 import string
 import socket
 
-def getNextTTL():
-	return 5
+max_ttl = 32
+min_ttl = 0
+
+def getNextTTL(ttl, icmp_type, icmp_code):
+	global max_ttl, min_ttl
+	if icmp_type == 3 and icmp_code == 3: # too long
+		max_ttl = ttl
+	elif icmp_type == 11 and icmp_code == 0: # too short
+		min_ttl = ttl
+	return (min_ttl + max_ttl) / 2
 
 def getRandomId():
 	return ''.join(random.choice(string.lowercase) for i in range(32))
@@ -20,26 +28,31 @@ def extractICMP(icmp_rsp):
 
 def main(dest_host):
 	dest_addr = socket.gethostbyname(dest_host)
-	port = 50254
-	while True:
+	port = 33465
+	ttl = 16
+	while ttl <= 64:
+		print "max_ttl: %s   min_ttl: %s  ttl: %s" % (max_ttl, min_ttl, ttl)
 		# set up sockets
 		sendSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		icmpSock = socket.socket(socket.AF_INET, socket.SOCK_RAW,
 			socket.IPPROTO_ICMP)
-		ttl = getNextTTL()
 		sendSock.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
 
 		# send a short message to the destination
 		identifier = getRandomId()
 		sendSock.sendto(identifier, (dest_addr, port))
 		
-		icmp_data = icmpSock.recv(512)
-		extractICMP(icmp_data)
-		# print icmp_data
-
+		icmp_rsp = icmpSock.recv(512)
+		(icmp_type, icmp_code, icmp_src, icmp_data) = extractICMP(icmp_rsp)
+	#	if icmp_data == identifier: # icmp message is from udp msg
+		ttl = getNextTTL(ttl, icmp_type, icmp_code)
+		
 		sendSock.close()
 		icmpSock.close()
-		break
+		
+		if min_ttl == max_ttl - 1:
+			break
+	print ttl + 1
 
 if __name__ == '__main__':
-	main("google.com")
+	main("yahoo.com")
